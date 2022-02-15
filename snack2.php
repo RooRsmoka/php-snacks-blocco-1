0be2fbd6f7ccd0ba6e8a1951cb9bf4be57224db3
RooRsmoka <chri.disa96@gmail.com>
2022-02-15 20:50:27 +0100

    checking the data entered by the user in the query

--- snack2.php
+++ snack2.php
@@ -2,5 +2,13 @@
 // Snack 2
 // Passare come parametri GET name, mail e age e verificare che name sia più lungo di
 // 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$age = $_GET["age"];
 // Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”.
+if (strlen($name) > 3 && strpos($mail, ".") && strpos($mail, "@") && is_numeric($age)) {
+    echo "<p>Accesso riuscito.</p>";
+} else {
+    echo "<p>Accesso negato</p>";
+}
 ?>
