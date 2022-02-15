16f63b1e892801770e932678b0ef7b23c334a9df
RooRsmoka <chri.disa96@gmail.com>
2022-02-16 00:45:54 +0100

    created loop that generates random numbers without repetitions

--- snack4.php
+++ snack4.php
@@ -2,4 +2,14 @@
 // Snack 4
 // Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere
 // lo stesso numero più di una volta.
+$numbers = [];
+
+while (count($numbers) < 15) {
+    $number = rand(1, 90);
+
+    if (!in_array($number, $numbers)) {
+        $numbers[] = $number;
+    }
+}
+// var_dump($numbers)
 ?>
