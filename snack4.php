633b52990b12be67e9bb075a91acf964f93ee709
RooRsmoka <chri.disa96@gmail.com>
2022-02-16 00:47:42 +0100

    print on page of the array of random numbers

--- snack4.php
+++ snack4.php
@@ -13,3 +13,22 @@ while (count($numbers) < 15) {
 }
 // var_dump($numbers)
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h2>I tuoi numeri fortunati:</h2>
+        <p>
+            <?php for($i = 0; $i < 15; $i++) {
+                echo "<div>". $numbers[$i] ."</div>";
+            }
+            ?> 
+        </p>
+    </body>
+</html>
